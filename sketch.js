4e2700c58ccdba7608a593616e02f898c03fcb30
amitkrprajapati <prajapatiamit76@gmail.com>
2022-01-10 16:11:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,26 @@
 function setup() {
-  createCanvas(400, 400);
-}
-  
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+    createCanvas(windowWidth, windowHeight);
+    strokeWeight(10); 
+    stroke('blue');
+    offset = 50
+    fixedPoint = [[windowWidth/2 , offset]
+                ,[offset, windowHeight - offset],
+              [windowWidth - offset, windowHeight - offset]]
+    point(fixedPoint[0][0], fixedPoint[0][1]);
+    point(fixedPoint[1][0], fixedPoint[1][1]);
+    point(fixedPoint[2][0], fixedPoint[2][1]);
+
+    v = [windowHeight/2 , windowHeight/2];
   }
-  ellipse(mouseX, mouseY, 80, 80);
-}
+  
+  function midPoint(point1, point2)
+  {
+    res = [(point1[0] + point2[0])/2 , (point1[1] + point2[1])/2]
+    return res;
+  }
+  function draw() {
+    rn = random([0,1,2]);
+    v = midPoint(v, fixedPoint[rn])
+    console.log(v)
+    point(v[0], v[1]);
+  }
