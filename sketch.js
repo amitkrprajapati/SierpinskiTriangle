b64b56891d0538db4639e8d0aa372e27c394801b
amitkrprajapati <prajapatiamit76@gmail.com>
2022-01-10 16:17:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 function setup() {
+    sz = min(windowWidth, windowHeight);
     createCanvas(windowWidth, windowHeight);
     strokeWeight(10); 
     stroke('blue');
-    offset = 50
-    fixedPoint = [[windowWidth/2 , offset]
-                ,[offset, windowHeight - offset],
-              [windowWidth - offset, windowHeight - offset]]
+    offset = 20
+    fixedPoint = [[sz/2 , offset]
+                ,[offset, sz - offset],
+              [sz - offset, sz - offset]]
     point(fixedPoint[0][0], fixedPoint[0][1]);
     point(fixedPoint[1][0], fixedPoint[1][1]);
     point(fixedPoint[2][0], fixedPoint[2][1]);
